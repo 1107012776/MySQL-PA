89ba4db17927e45f86b6a8ec391364ead43ef72a
linyushan <linyushan@nawang.cn>
2021-12-29 12:39:47 +0800

    mysql pa开发

---
+++ main.go
@@ -0,0 +1,7 @@
+package MySQL_PA
+
+import "MySQL-PA/mysql_pa"
+
+type Explain struct {
+	mysql_pa.Explain
+}
--- tests/main_test.go
+++ tests/main_test.go
@@ -1,18 +1,18 @@
 package tests
 
 import (
-	"MySQL-PA/mysql_pa"
+	"MySQL-PA"
 	assert "github.com/magiconair/properties/assert"
 	"testing"
 )
 
 func Test_Analyze(t *testing.T) {
-	p := mysql_pa.Explain{}
+	p := MySQL_PA.Explain{}
 	assert.Equal(t, p.Analyze(), true)
 }
 
 func Test_GetDb(t *testing.T) {
-	p := mysql_pa.Explain{}
+	p := MySQL_PA.Explain{}
 	db := p.GetDb("phpshardingpdo1")
 	assert.Equal(t, db != nil, true)
 }
